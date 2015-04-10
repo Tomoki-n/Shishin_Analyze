bbc40a18c5554c3bd66d953ceee2ea77f55dfe9b
Nishinaka Tomoki <ariaqua713.f7z@gmail.com>
2015-04-10 16:46:49 +0900

    Add Ai Class

---
+++ src/com/company/AI.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by tomoki-n on 2015/04/10.
+ */
+public class AI {
+}
--- src/com/company/AI_Nishinaka.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-/**
- * Created by tomoki-n on 4/10/15.
- */
-public class AI_Nishinaka {
-}
