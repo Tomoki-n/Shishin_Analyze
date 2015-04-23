9edb947cb68039871e1655cf8e01b1627fa01d04
hosono yuto <yu.to.h3121@gmail.com>
2015-04-23 15:07:34 +0900

    Add some method

--- src/com/company/Boardinfo.java
+++ src/com/company/Boardinfo.java
@@ -34,9 +34,51 @@ public class Boardinfo {
     /** 現在のボードのタイプ */
     private int boardType = BOARD_TYPE_UNDEFINED;
 
+    /** 勝っているチームのID */
+    private int victoryTeamID = -1;
+
+
+    /** 二点間の距離 */
+    public int distance(Point a, Point b) {
+        if(a.x == b.x) {
+            return Math.abs(a.y - b.y);
+        } else if(a.y == b.y) {
+            return Math.abs(a.x - b.x);
+        } else {
+            int xdef = Math.abs(a.x - b.x);
+            int ydef = Math.abs(a.y - b.y);
+            return xdef > ydef?xdef:ydef;
+        }
+    }
+
     /** ユニット位置を渡す */
     public Point[][] getUnitLocation() {
         return unitLocation;
     }
 
+    /** 得点差 */
+    public int getPoint(){
+        int i,j;
+        i=teamPoint[0]-teamPoint[1];
+        if(i>0)  victoryTeamID=0;
+        else if(i<0)  victoryTeamID=1;
+
+        return Math.abs(i);
+    }
+
+    /** 最も近いタワー */
+    public Point[][] gettower(Point[][] pos){
+        Point t1 = Point[1][4];
+        Point t2 = Point[4][4];
+        Point t3 = Point[7][4];
+
+        int i,j,k;
+        i=distance(Point pos,Point t1);
+        j=distance(Point pos,Point t2);
+        k=distance(Point pos,Point t3);
+
+        if(i<j&&i<k)  return(t1);
+        else if(j<i&&j<k)  return(t2);
+        else if(k<i&&k<j)  return(t3);
+    }
 }
