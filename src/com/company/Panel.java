e84e63e1fd97af4432ee470d354be4901c7793ac
Tomoki Nishinaka <ariaqua713.f7z@gmail.com>
2015-04-26 17:21:12 +0900

    Add Panel Class

---
+++ src/com/company/Panel.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * Created by tomoki-n on 4/26/15.
+ */
+public class Panel {
+
+    /** 評価後の得点 */
+    public int score;
+
+    /** 敵の駒の状態 */
+    public Boolean ENEMY_BLUE   = false;
+    public Boolean ENEMY_GREEN  = false;
+    public Boolean ENEMY_RED    = false;
+    public Boolean ENEMY_YELLOW = false;
+
+    /** 味方の駒の状態 */
+    public Boolean ALLY_BLUE   = false;
+    public Boolean ALLY_GREEN  = false;
+    public Boolean ALLY_RED    = false;
+    public Boolean ALLY_YELLOW = false;
+
+}
+
