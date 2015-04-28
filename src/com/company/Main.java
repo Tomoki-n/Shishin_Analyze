ab7ba4f4deddf8ebb58395713b2c8384e7d9f3be
Nishinaka Tomoki <ariaqua713.f7z@gmail.com>
2015-04-28 17:11:18 +0900

    change main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+import com.sun.tools.javac.comp.Check;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -8,6 +8,8 @@ public class Main {
 
     private static String addr;
     private static String ai1;
+    private static String type1;
+
     private static int GAME_STATUS = 0;
 
     public static void main(String[] args) {
@@ -15,7 +17,7 @@ public class Main {
         System.out.println("Starting Program");
 
 
-      for (int i=0; i<2; i++){
+        for (int i=0; i<2; i++){
             switch (GAME_STATUS) {
                 case 0: {
                     addr = init();
@@ -28,7 +30,21 @@ public class Main {
                     GAME_STATUS = 2;
                 }
                 case 2: {
-                    AI ai = new AI(addr,ai1);
+                    if (ai1 == "1"){
+                        AI ai = new AI(addr,ai1,type1);
+                    }
+                    else if (ai1 == "2"){
+                        AI1 ai = new AI1(addr,ai1,type1);
+                    }
+                    else if (ai1 == "3"){
+                        AI2 ai = new AI2(addr,ai1,type1);
+                    }
+                    else if (ai1 == "4"){
+                        AI3 ai = new AI3(addr,ai1,type1);
+                    }
+                    else {
+                        AI ai = new AI(addr, ai1,type1);
+                    }
                     break;
                 }
 
@@ -45,13 +61,16 @@ public class Main {
 
         String address =null;
 
-        System.out.print("input IP Address: ");
+        System.out.println("input IP Address: ");
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
 
         try{
-            addr = br.readLine();
 
+            addr = br.readLine();
+            if(!checkIP(addr)){
+                init();
+            }
         }catch(Exception e){
             init();
         }
@@ -63,13 +82,13 @@ public class Main {
 
     public static String AI_type(){
         String type = null;
-        System.out.print("AI:1.Higuchi 2.Nishinaka 3.Hosono 4.Demo \n");
-        System.out.print("input AI_NUMBER: ");
+        System.out.println("AI:1.Higuchi 2.Nishinaka 3.Hosono 4.Demo \n");
+        System.out.println("input AI_NUMBER: ");
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
 
         try{
-             ai1 = br.readLine();
+            ai1 = br.readLine();
 
         }catch(Exception e){
             AI_type();
@@ -78,4 +97,29 @@ public class Main {
         return type;
     }
 
+    public static boolean checkIP(String str) {
+        String pattern = "^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$";
+        return str.matches(pattern);
+    }
+
+    public static String Connect_type(){
+        String type = null;
+        System.out.println("0:CONNECT 1:RECONNECT \n");
+        System.out.println("input NUMBER: ");
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        try{
+            type = br.readLine();
+
+        }catch(Exception e){
+            AI_type();
+        }
+        type = ai1;
+        return type;
+    }
+
+
 }
+
+
