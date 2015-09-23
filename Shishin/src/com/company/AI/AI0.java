fbc36fa5ac6098bba8563aef2ed03126786c8c55
Yuta Higuchi <mudai.dotx.syndrome@gmail.com>
2015-09-24 03:04:03 +0900

    AI0 *WIP*

--- Shishin/src/com/company/AI/AI0.java
+++ Shishin/src/com/company/AI/AI0.java
@@ -1,13 +1,7 @@
 package com.company.AI;
 
-
-import com.company.AI.AI;
-
 import java.awt.*;
 import java.io.IOException;
-import java.nio.file.attribute.PosixFileAttributes;
-import java.util.ArrayList;
-import java.util.IntSummaryStatistics;
 
 
 /**
@@ -16,8 +10,8 @@ import java.util.IntSummaryStatistics;
 public class AI0 extends AI {
 
     //序盤時に先に動くユニットの番号
-    private int preunit_l = -1;
-    private int preunit_r = -1;
+    private int preUnit_l = -1;
+    private int preUnit_r = -1;
 
     //現在どの戦術で動くかを指定(今のところ使ってない)
     private int mode = MODE_OPENING;
@@ -25,8 +19,18 @@ public class AI0 extends AI {
     private static final int MODE_OPENING = 11;
 
     //ターン後半でどのような動きをとるかの指定
-    private int turnmode = -1;
-    private static final int TM_DEFEND = 10;
+    private int turnMode = -1;
+    private static final int TM_DIRECT = 10;    //前半の手を指す時点で後半の手を決定した場合。tempUnit, tempPosの指定が必要
+    private static final int TM_DEFEND = 20;    //前半終了時点で、2つの柱に自軍の駒が2体と1体居り、その1体のみで守られる柱から距離1の場所に残りの1体が居る状態
+    private static final int TM_SINGLE = 21;    //ターン後半で単独行動をとる場合。tempUnitとdestinationの指定が必要。
+    
+    //一時的な指し手指示用
+    //実際の使い方は対応するturnMode同士を見比べてくださいな
+    private Point destination = null;
+    private int tempUnit = -1;
+    private Point tempPos = null;
+
+
 
     /**
      * Creates new form GameField
@@ -43,7 +47,7 @@ public class AI0 extends AI {
         if (msg == "Select Unit") {
 
             //chat送信
-            //さすがにネタなので、ここの実装は本編が片付いてからで…
+            //さすがにネタ要素なので、ここの実装は本編が片付いてからで…
             /*
             if(Math.random() <= (5.0 / 18.0)) {
                 this.sthread.sendMessage("600 " + Glyph.getGlyphSeq());
@@ -60,34 +64,25 @@ public class AI0 extends AI {
     /**
      * 一応核となる部分ではありますが、ここでは分岐させているだけです
      * 詳しい挙動は各関数にて
-     * @return
+     * @return 指し手を指定する配列
      */
     private synchronized int[] move_main() {
         int[] move = new int[3];
         if(this.turnCount == 1) {
-            move = move_first();
+            //1手目は特殊指定
+            move = move_firstTurn();
         } else {
+            //指示処理をターンの前後半で分ける
             switch (this.turnState) {
                 case STATE_PLAY_TURN1:
                 case STATE_PLAY_TURN2:
-                    move = move_firsthalf();
+                    move = move_firstHalf();
                     break;
                 case STATE_PLAY_TURN3:
                 case STATE_PLAY_TURN4:
-                    move = move_secondhalf();
+                    move = move_secondHalf();
                     break;
             }
-            /*
-            //一旦没
-            switch (this.mode) {
-                case MODE_DEFEND:
-                    move = move_defend();
-                    break;
-                case MODE_OPENING:
-                    move = move_opening();
-                    break;
-            }
-            */
         }
 
         return move;
@@ -95,9 +90,9 @@ public class AI0 extends AI {
 
     /**
      * 1ターン目の挙動
-     * @return
+     * @return 指し手を指定する配列
      */
-    private synchronized int[] move_first() {
+    private synchronized int[] move_firstTurn() {
         int[] move = new int[3];
 
         if(this.turnCount != 1) {
@@ -113,10 +108,10 @@ public class AI0 extends AI {
                 move[0] = (int)(Math.random() * 4.0);
                 if((int)(Math.random() * 2.0) == 0) {
                     move[1] = 3;
-                    preunit_l = move[0];
+                    preUnit_l = move[0];
                 } else {
                     move[1] = 5;
-                    preunit_r = move[0];
+                    preUnit_r = move[0];
                 }
                 move[2] = 6;
                 break;
@@ -129,38 +124,38 @@ public class AI0 extends AI {
                  */
                 for(int unit = 0; unit < 4; unit++) {
                     if(!this.unitLocation[0][unit].equals(Base0)) {
-                        Point enemypos = this.unitLocation[0][unit];
+                        Point enemyPos = this.unitLocation[0][unit];
 
-                        if(enemypos.y == 8) {
+                        if(enemyPos.y == 8) {
                             //後ろ
                             move[0] = loseUnit1(unit);
                             if((int)(Math.random() * 2.0) == 0) {
                                 move[1] = 3;
-                                preunit_l = move[0];
+                                preUnit_l = move[0];
                             } else {
                                 move[1] = 5;
-                                preunit_r = move[0];
+                                preUnit_r = move[0];
                             }
                         } else {
                             //前か横
                             move[0] = winUnit1(unit);
-                            switch (enemypos.x) {
+                            switch (enemyPos.x) {
                                 case 3:
                                     move[1] = 3;
-                                    preunit_r = move[0];
+                                    preUnit_r = move[0];
                                     break;
                                 case 4:
                                     if((int)(Math.random() * 2.0) == 0) {
                                         move[1] = 3;
-                                        preunit_l = move[0];
+                                        preUnit_l = move[0];
                                     } else {
                                         move[1] = 5;
-                                        preunit_r = move[0];
+                                        preUnit_r = move[0];
                                     }
                                     break;
                                 case 5:
                                     move[1] = 3;
-                                    preunit_r = move[0];
+                                    preUnit_r = move[0];
                                     break;
                                 default:
                                     System.out.println("Error: 先手がありえない手を指した");
@@ -178,23 +173,24 @@ public class AI0 extends AI {
                  * 最初の駒の移動と逆方向前側に、最初に前に出した駒と対にならない駒を移動
                  */
 
+                int preUnit;        //最初に動かした駒の種類
+                boolean preLeft;    //最初に左前に駒を出せばtrue, 右前に駒を出せばfalse
+
                 //最初に動かした方向によって、次に動かす方向が決定
-                int preunit;
-                boolean preleft;
-                if(this.preunit_l != -1) {
-                    preunit = preunit_l;
-                    preleft = true;
+                if(this.preUnit_l != -1) {
+                    preUnit = preUnit_l;
+                    preLeft = true;
                     move[1] = 5;
                 } else {
-                    preunit = preunit_r;
-                    preleft = false;
+                    preUnit = preUnit_r;
+                    preLeft = false;
                     move[1] = 3;
                 }
 
                 //最初に動かした
                 move[2] = 2;
 
-                switch (preunit) {
+                switch (preUnit) {
                     case GREEN:
                     case RED:
                         if((int)(Math.random() * 2.0) == 0) {
@@ -213,10 +209,10 @@ public class AI0 extends AI {
                         break;
                 }
 
-                if(preleft) {
-                    preunit_r = move[0];
+                if(preLeft) {
+                    preUnit_r = move[0];
                 } else {
-                    preunit_l = move[0];
+                    preUnit_l = move[0];
                 }
                 break;
             case STATE_PLAY_TURN4:
@@ -238,7 +234,7 @@ public class AI0 extends AI {
     }
 
 
-    private synchronized int[] move_firsthalf() {
+    private synchronized int[] move_firstHalf() {
         /*
          * ターン前半を指す
          * muzui.
@@ -249,12 +245,14 @@ public class AI0 extends AI {
         if(existTower(this.MyTeamID) == 4) {
             //既に2つ柱を持っている場合
             //ランダムな柱からランダムな駒を一時的にランダムな方向に移動
+            //ターン後半にて動かした駒をもと居た柱に戻すので、どの方向にどの駒が動こうが関係ない
 
-            turnmode = TM_DEFEND;
+            turnMode = TM_DEFEND;
             move[0] = (int) (Math.random() * 4.0);
             int direction = (int)(Math.random() * 8.0);
             Point tower = getNearestTower(this.unitLocation[this.MyTeamID][move[0]]);
 
+            //わざわざランダム要素入れる必要もないですが、在った方が面白いじゃん？って話です
             switch (direction) {
                 case 0:
                 case 3:
@@ -290,23 +288,28 @@ public class AI0 extends AI {
             }
         } else if(existTower(this.MyTeamID) == 3) {
             //既に3つの駒が柱にいる場合
-            Point tower;
-            if(this.existUnit(tower_center, this.MyTeamID) == 1) {
-                tower = tower_center;
-            } else if(this.existUnit(tower_left, this.MyTeamID) == 1) {
+            Point tower;    //目的地の柱
+            if(this.existUnit(tower_left, this.MyTeamID) == 1) {
                 tower = tower_left;
+            } else if(this.existUnit(tower_center, this.MyTeamID) == 1) {
+                tower = tower_center;
             } else {
                 tower = tower_right;
+                
             }
 
             for (int unit = 0; unit < 4; unit++) {
                 Point pos = this.unitLocation[this.MyTeamID][unit];
-
+                
+                //まだどの柱の上にも居ない駒を次に動かす
                 if (pos != tower_center && pos != tower_left && pos != tower_right) {
                     move[0] = unit;
 
+                    //柱までの距離に応じ移動先の位置を指定
                     if(distance(pos, tower) == 1) {
-                        //そのまま柱に入ってしまうと、ターン後半にて一度柱から出る必要が出てしまうため
+                        turnMode = TM_DEFEND;
+                        
+                        //そのまま柱に入ってしまうと、ターン後半にてどれかの駒が柱から出る必要が出てしまうため
                         //一時的に場所をずらす(一手分損だが仕方ない)
                         if(pos.x == tower.x) {
                             if((int)(Math.random() * 2.0) == 0) {
@@ -327,12 +330,54 @@ public class AI0 extends AI {
                         } else {
                             move[2] = tower.y;
                         }
-
-                        turnmode = TM_DEFEND;
-                    } else {
+                    } else if(distance(pos, tower) == 2) {                        
+                        //陣地から一歩進んだ状態
                         //柱に近づく
-                        //このときのturnmodeまだ決めてない
-                        //TODO:
+                        //次の移動箇所は柱との距離が1になる場所からランダムに選ばれる
+                        turnMode = TM_DEFEND;
+
+                        //次の移動位置として使用できるかどうか
+                        boolean[][] isNext = new boolean[3][3];
+
+                        for(int x = 0; x < 3; x++) {
+                            for(int y = 0; y < 3; y++) {
+                                Point nextPos = new Point(pos.x + x - 1, pos.y + y - 1);
+                                isNext[x][y] = (distance(nextPos, tower) == 1);
+                            }
+                        }
+
+                        while (true) {
+                            int x = (int)(Math.random() * 3);
+                            int y = (int)(Math.random() * 3);
+                            if(isNext[x][y]) {
+                                move[1] = pos.x + x - 1;
+                                move[2] = pos.y + y - 1;
+                                break;
+                            }
+                        }
+                    } else {
+                        //残る1駒が自陣に残っている場合
+                        tempUnit = move[0];
+                        destination = tower;
+                        turnMode = TM_SINGLE;
+
+                        if(tower.equals(tower_left)) {
+                            move[1] = 3;
+                        } else if(tower.equals(tower_right)) {
+                            move[1] = 5;
+                        } else {
+                            if((int)(Math.random() * 2) == 0) {
+                                move[1] = 3;
+                            } else {
+                                move[1] = 5;
+                            }
+                        }
+
+                        if(this.MyTeamID == 0) {
+                            move[2] = 6;
+                        } else {
+                            move[2] = 2;
+                        }
                     }
 
                     break;
@@ -347,13 +392,13 @@ public class AI0 extends AI {
         return move;
     }
 
-    private synchronized int[] move_secondhalf() {
+    private synchronized int[] move_secondHalf() {
         assert this.turnState == STATE_PLAY_TURN3 || this.turnState == STATE_PLAY_TURN4;
 
         int[] move = new int[3];
-        switch (turnmode) {
+        switch (turnMode) {
             case TM_DEFEND:
-                //2つの柱を守っている状態から、一歩退避させた状態
+                //2つの柱を守っている状態から、一歩退避させた状態及び、それと同じ状況
                 //2つの柱を守っている状態に戻す
                 assert existTower(this.MyTeamID) == 3;
 
@@ -369,21 +414,35 @@ public class AI0 extends AI {
                     }
                 }
                 break;
+            case TM_DIRECT:
+                move[0] = tempUnit;
+                move[1] = tempPos.x;
+                move[2] = tempPos.y;
+                break;
+            case TM_SINGLE:
+//TODO
+                break;
+
             //TODO:残り(前半の手が決まり次第)
+            default:
+                System.out.println("Error: turnModeが指定されていない");
         }
 
-        //turnmodeのリセット
-        turnmode = -1;
+        //ターン後半指定用一時変数の初期化
+        turnMode = -1;
+        tempUnit = -1;
+        tempPos = null;
         return move;
     }
 
+    
     /* 以下utility
      * 親クラスであるAIの方に移植してもOK
+     * (実際、いくつかのutility関数はAIクラスに移植した)
      */
-
-
+    
     /**
-     * 指定したチームの駒のうち、何個が柱に居るか
+     * 指定したチームの駒のうち、何個が柱に居るか(どの柱かは関係ない)
      * @param teamID 0ならば1ターン目先攻チーム、1ならば1ターン目後攻チーム、それ以外なら両チーム合計
      * @return 指定したチームの駒のうち、何個が柱に居るか(0～4)
      */
